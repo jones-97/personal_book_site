6e2372850894aff7fae846fcd70b92f471135040
Jones Mghanga <jonesmwaniki@ymail.com>
2025-02-19 13:28:09 +0300

    Some changes here and there; site functioning well; password recovery left

--- routes/chapterRoutes.js
+++ routes/chapterRoutes.js
@@ -38,6 +38,46 @@ router.get('/book/:bookId', async (req, res) => {
     }
 });
 
+//Get list of chapter Ids; for simplicity instead of getting the whole chapter details
+//Is this safe? Is this secure? We don't know. Check back on it.
+router.get('/ids/:bookId', async(req, res) => { 
+	  try {
+        const chapters = await Chapter.find({ bookId: req.params.bookId }, '_id');
+        const chapterIds = chapters.map(chapter => chapter._id.toString());
+		console.log("Chapter Ids: ", chapterIds);
+        res.json(chapterIds);
+		console.log("Suc cess in getting chapter Ids");
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+		console.log("Failed getting ids of chapters");
+    }
+	
+});
+
+
+//Get latest chapter of a book
+router.get('/book/:bookId/latest', async (req, res) => {
+	const { bookId } = req.params;	
+	
+	try {
+		
+		const latestChapter = await Chapter.findOne({ bookId }, 'title')
+            .sort({ _id: -1 }) // Sort by _id in descending order (latest first)
+            .limit(1); // Get only the latest chapter
+
+        if (!latestChapter) {
+            return res.status(404).json({ message: 'No chapters found for this book' });
+        }
+		console.log("The latest chapter is: ", latestChapter.title);
+        res.json(latestChapter.title || { title: 'No chapters yet' });	
+		
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+		console.log("failed getting latest chapter");
+}
+}
+);
+
 //Create chapters by book value (kinda recommended)
 router.post('/', authMiddleware, async (req, res) => {
 	try {
